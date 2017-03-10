8e171209c1467f8db51639a011df8c11e103b234
Your Name <you@example.com>
2017-03-10 17:15:53 +0530

    square

--- week2/squa.c
+++ week2/squa.c
@@ -1,11 +1,17 @@
 #include<stdio.h>
 #include<math.h>
+float square(float n);
 int main()
 {
 float n,s;
 printf("enter the number");
 scanf("%f",&n);
-s=sqrt(n);
+s=square(n);
 printf("square root  of %f=%f",n,s);
 return 0;
 }	
+float square(float n)
+{
+float s=sqrt(n);
+return s;
+}
